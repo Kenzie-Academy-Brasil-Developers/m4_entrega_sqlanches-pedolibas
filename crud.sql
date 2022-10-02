856a72d2d531f93cf606a640061578c0ccef6669
Pedro Gabriel <pedrogv03@hotmail.com>
2022-10-02 18:48:54 -0300

    feat: bd

--- crud.sql
+++ crud.sql
@@ -1,34 +1,70 @@
--- Aqui você deve colocar os códigos SQL referentes às
--- Simulações de um CRUD
-
 -- Criação
 
 -- 1)
 
-
+INSERT INTO clientes
+  (nome, lealdade)
+VALUES
+  ('Georgia', 0);
 
 -- 2)
 
-
+INSERT INTO pedidos
+  (status, cliente_id)
+VALUES
+  ('Recebido', 6);
 
 -- 3)
 
-
+INSERT INTO produtos_pedidos
+          (pedido_id, produto_id)
+VALUES
+          (6, 1),
+          (6, 2),
+          (6, 6),
+          (6, 8),
+          (6, 8);
 
 -- Leitura
 
 -- 1)
 
+SELECT
+  pedidos.id,
+  clientes.nome,
+  clientes.lealdade,
+  pedidos.id,
+  pedidos.status,
+  pedidos.cliente_id,
+  produtos.id,
+  produtos.nome,
+  produtos.tipo,
+  produtos.preco,
+  produtos.pts_de_lealdade
+FROM
+  produtos
+  INNER JOIN produtos_pedidos 
+  ON produtos.id = produtos_pedidos.produto_id
+  INNER JOIN pedidos 
+  ON produtos_pedidos.pedido_id = pedidos.id
+  INNER JOIN clientes 
+  ON pedidos.cliente_id = clientes.id WHERE clientes.id = 6;
 
 
 -- Atualização
 
 -- 1)
 
-
+UPDATE 
+  clientes
+SET 
+  lealdade = 48
+WHERE
+  id = 6;
 
 -- Deleção
 
 -- 1)
 
-
+DELETE FROM
+  clientes WHERE nome = 'Marcelo';
--- selects.sql
+++ selects.sql
@@ -1,12 +1,64 @@
--- Aqui você deve colocar os códigos SQL referentes às
--- Seleções de dados
-
 -- 1)
+
+SELECT
+  pedidos.id,
+  pedidos.status,
+  pedidos.cliente_id,
+  produtos.id,
+  produtos.nome,
+  produtos.tipo,
+  produtos.preco,
+  produtos.pts_de_lealdade
+FROM
+  produtos_pedidos
+  INNER JOIN pedidos
+  ON produtos_pedidos.pedido_id = pedidos.id
+  INNER JOIN produtos
+  ON produtos_pedidos.produto_id = produtos.id;
 
 -- 2)
 
+SELECT
+  pedidos.id
+FROM
+  produtos_pedidos
+  INNER JOIN pedidos
+  ON produtos_pedidos.pedido_id = pedidos.id
+  INNER JOIN produtos
+  ON produtos_pedidos.produto_id = produtos.id WHERE nome LIKE 'Fritas';
+
 -- 3)
+
+SELECT
+  clientes.nome as gostam_de_fritas
+FROM
+  clientes
+  INNER JOIN pedidos 
+  ON clientes.id = pedidos.cliente_id
+  INNER JOIN produtos_pedidos 
+  ON pedidos.id = produtos_pedidos.pedido_id
+  INNER JOIN produtos 
+  ON produtos_pedidos.produto_id = produtos.id WHERE produtos.nome = 'Fritas';
 
 -- 4)
 
+SELECT
+  SUM(produtos.preco)
+FROM
+  produtos
+  INNER JOIN produtos_pedidos 
+  ON produtos.id = produtos_pedidos.produto_id
+  INNER JOIN pedidos 
+  ON produtos_pedidos.pedido_id = pedidos.id
+  INNER JOIN clientes 
+  ON pedidos.cliente_id = clientes.id WHERE clientes.nome = 'Laura';
+
 -- 5)
+
+SELECT
+  produtos.nome,
+  COUNT(produtos_pedidos.produto_id)
+FROM
+ produtos
+ INNER JOIN produtos_pedidos ON produtos_pedidos.produto_id = produtos.id
+ GROUP BY produtos.nome;
